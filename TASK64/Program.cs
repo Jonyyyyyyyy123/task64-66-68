52eaa182cb8ac3495fc378fecf336323048c313d
EVGENIY <kozubov89@bk.ru>
2023-02-19 11:17:59 +0300

    task64,66,68

--- TASK64/Program.cs
+++ TASK64/Program.cs
@@ -2,21 +2,21 @@
 // N = 5. -> "5, 4, 3, 2, 1"
 // N = 8. -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.Write("Введите число: ");
-int == Convert.ToInt32(Console.ReadLine());
-1  == 1;
-NaturalToLow(number, count);
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
 
-
-void NaturalToLow(int n, int count)
+void PrintNumber(int n, int count)
 {
-    if (count > n)
-    {
-        return;
-    }
-    else
-    {
-        NaturalToLow(n, count  + 1);
-        Console.Write (count  + " ");
-    }
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
 }
